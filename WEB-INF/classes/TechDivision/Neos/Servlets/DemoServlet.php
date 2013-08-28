8cc5bf99143b25e149522932ef41874e8bef9c93
Tim Wagner <t.wagner@techdivision.com>
2013-08-28 17:32:43 +0200

    Initial commit

---
+++ WEB-INF/classes/TechDivision/Neos/Servlets/DemoServlet.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * TechDivision\Neos\Servlets\NeosServlet
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ */
+
+namespace TechDivision\Neos\Servlets;
+
+use TechDivision\ServletContainer\Interfaces\Request;
+use TechDivision\ServletContainer\Interfaces\Response;
+use TechDivision\ServletContainer\Servlets\HttpServlet;
+
+/**
+ * @package     TechDivision\Neos
+ * @copyright   Copyright (c) 2013 <info@techdivision.com> - TechDivision GmbH
+ * @license     http://opensource.org/licenses/osl-3.0.php
+ *              Open Software License (OSL 3.0)
+ * @author      Johann Zelger <jz@techdivision.com>
+ */
+
+class NeosServlet extends HttpServlet
+{
+}
